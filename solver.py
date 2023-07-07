f2b0aef4f23ce8fae6fed2849a3c82b2de7d9000
Vlad <pvlad1285@gmail.com>
2023-07-07 15:56:21 +0300

    Solver

---
+++ solver.py
@@ -0,0 +1,30 @@
+from api import API
+from config import API_TOKEN
+
+
+class Solver():
+    def __init__(self, api: API):
+        self.api = api
+    
+    def solve(self, data: dict):
+        return {}
+    
+    def solve_problem(self, id: int):
+        problem = self.api.get_problem(id)
+        
+        data = self.solve(problem)
+        
+        return self.api.submit(id, data)
+    
+    def solve_all(self):
+        amount = self.api.get_promblems_count()
+        
+        for i in range(1, amount + 1):
+            print(f'Problem {i} solved.')
+
+
+if __name__ == '__main__':
+    api = API(API_TOKEN)
+    solver = Solver(api)
+    
+    solver.solve_all()
